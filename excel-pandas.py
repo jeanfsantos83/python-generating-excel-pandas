9bb304462dd0baed70140b1a522acaacfd4660f0
jeanfsantos83 <jeanjfs5326@gmail.com>
2024-05-06 19:22:36 -0300

    commit files

---
+++ excel-pandas.py
@@ -0,0 +1,12 @@
+# pip install pandas #
+# pip install openpyxl #
+import pandas as pd
+
+carros = {
+    'marca': ['Fiat', 'Chevrolet', 'Ford'],
+    'modelo': ['Marea', 'Chevette', 'Escort'],
+    'ano': ['1999', '1978', '1995'],
+}
+
+dataframe = pd.DataFrame(carros)
+dataframe.to_excel('./carros.xlsx')
